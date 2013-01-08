b58b889eae9ac00dac6c95995510667c8f65a6d3
mirraz <mirraz@example.com>
2013-01-08 20:00:18 +0600

    remove unused blocks.c, codepages.? regfile.c: remove old *_print_* code

--- src/codepages.h
+++
@@ -1,8 +0,0 @@
-#ifndef CODEPAGES_H_
-#define CODEPAGES_H_
-
-#include <stdio.h>
-
-void fprintStringUnicode(FILE *fout, const unsigned char *str_words, unsigned int len);
-
-#endif /* CODEPAGES_H_ */
